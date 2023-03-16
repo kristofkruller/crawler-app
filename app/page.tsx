515e8396e9e69168b938df94230030e185c6ccc9
Kristof Kruller <kristof.kruller@gmail.com>
2023-03-16 19:51:19 +0100

    &apos;

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <div className={styles.sec}><h1>Scraper</h1></div>
           <div className={styles.thr}><h1>Spider</h1></div>
         </div>
-        <p>for the top 50's</p>
+        <p>for the top 50&apos;s</p>
         <h2>Just pick one site from the list and let the <span>crawler</span> do his job</h2>
       </section>
       {/* <section className={styles.crawler}>
