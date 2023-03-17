3e1de06a2f5f69d15eea794bb6235956a8f6ecf4
Kristof Kruller <kristof.kruller@gmail.com>
2023-03-17 16:30:28 +0100

    clear build obstacles

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
 import styles from '@root/styles/home.module.scss'
 import Table from '@root/components/Table/table'
 import SiteData from "@root/assets/data/dataset.json";
 import SearchBox from '@root/components/Search/SearchBox';
 
-export default function Home({children}: React.PropsWithChildren) {
+export default function Home() {
   return (
     <main className={styles.wrap}>
       <section className={styles.title}>
@@ -17,16 +16,8 @@ export default function Home({children}: React.PropsWithChildren) {
         <p>for the top 50&apos;s</p>
         <h2>Just pick one site from the list and let the <span>crawler</span> do his job</h2>
       </section>
-      {/* <section className={styles.crawler}>
-        <Image
-        alt='spider with orange background represents the phrase "crawler"'
-        src={"https://images.unsplash.com/photo-1611763806287-7f9858f4da69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"}
-        width={682}
-        height={455}
-        />
-      </section> */}
       <SearchBox data={SiteData} />
-      <Table data={SiteData}>{children}</Table>
+      <Table data={SiteData} />
     </main>
   )
 }
